fab7722650be9573962ac9456684744658159bbd
m95456749-glitch <m95456749@gmail.com>
2026-09-03 22:08:14 +0330

    Create images.js

---
+++ images.js
@@ -0,0 +1,36 @@
+const ZHENO_IMAGES = {
+  products: {
+    jelly: {
+      pomegranate: "IMG_20260903_014158_817.jpg",
+      strawberry: "IMG_20260903_014203_418.jpg",
+      peach: "IMG_20260903_014205_466.jpg",
+      raspberry: "IMG_20260903_014206_913.jpg",
+      blueberry: "IMG_20260903_014211_342.jpg",
+      orange: "IMG_20260903_014213_348.jpg",
+      pineapple: "IMG_20260903_014214_902.jpg",
+      sourCherry: "IMG_20260903_014218_054.jpg"
+    },
+
+    custard: {
+      banana: "IMG_20260903_014713_645.jpg",
+      melon: "IMG_20260903_014715_534.jpg",
+      strawberry: "IMG_20260903_014717_148.jpg",
+      cocoa: "IMG_20260903_014718_671.jpg",
+      sevenFruits: "IMG_20260903_014720_798.jpg",
+      orange: "IMG_20260903_014722_619.jpg",
+      vanilla: "IMG_20260903_014723_913.jpg"
+    }
+  },
+
+  gallery: [
+    "IMG_20260903_002041.jpg",
+    "IMG_20260903_002115.jpg",
+    "IMG_20260903_002139.jpg",
+    "IMG_20260903_002159.jpg",
+    "IMG_20260903_002226.jpg",
+    "IMG_20260903_002256.jpg",
+    "IMG_20260903_002320.jpg",
+    "IMG_20260903_002353.jpg",
+    "IMG_20260903_002418.jpg"
+  ]
+};
